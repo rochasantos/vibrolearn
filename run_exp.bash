22bc59c5e1817e711fb767d97b8e1aeeec1f242e
Francisco Boldt <fboldt@gmail.com>
2026-04-14 18:43:19 -0300

    Fix output directory in experiment script; change from 'experiments' to 'results' for consistency

--- run_exp.bash
+++ run_exp.bash
@@ -3,7 +3,7 @@
 # Run the experiment with the specified parameters
 for i in {1..10}; do
     for setup in dataset/cwru/*.json; do
-        python main.py -e $setup -c RandomForestClassifier > experiments/$(basename $setup .json)_${i}.txt 
-        python main.py -e $setup -a -c RandomForestClassifier > experiments/$(basename $setup .json)_${i}_aug.txt
+        python main.py -e $setup -c RandomForestClassifier > results/$(basename $setup .json)_${i}.txt 
+        python main.py -e $setup -a -c RandomForestClassifier > results/$(basename $setup .json)_${i}_aug.txt
     done
 done
